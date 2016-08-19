1581fad6503a6795f44e2d45ee5d5156f892a279
Steve Tan <steve.tan@ninjavan.co>
2016-08-19 18:34:56 +0800

    Enable error debug screen

--- src/main/java/me/stevetan/sparkpoc/Application.java
+++ src/main/java/me/stevetan/sparkpoc/Application.java
@@ -3,6 +3,7 @@ package me.stevetan.sparkpoc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
 
 /**
  * Created by stevetan on 19/8/16.
@@ -12,8 +13,20 @@ public class Application {
     final static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        port(8080);
+        bootstrap();
 
         get("/hello", (req, res) -> "Hello World");
+
+        get("/error", (req, res) -> {
+            throw new Exception("What?");
+        });
+    }
+
+    private static void bootstrap() {
+        // TODO
+        port(8080);
+
+        // Add this line to your project to enable the debug screen
+        enableDebugScreen();
     }
 }
