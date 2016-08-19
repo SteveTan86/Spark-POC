91042f8260b3a9aed4ffd992766143a26158f9fc
Steve Tan <steve.tan@ninjavan.co>
2016-08-19 18:20:24 +0800

    Tweak

--- src/main/java/me/stevetan/sparkpoc/Application.java
+++ src/main/java/me/stevetan/sparkpoc/Application.java
@@ -1,11 +1,19 @@
 package me.stevetan.sparkpoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import static spark.Spark.*;
+
 /**
  * Created by stevetan on 19/8/16.
  */
 public class Application {
+
+    final static Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        port(8080);
+
         get("/hello", (req, res) -> "Hello World");
     }
 }
