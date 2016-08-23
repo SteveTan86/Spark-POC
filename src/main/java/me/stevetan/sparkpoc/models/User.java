aef6d1feacf34b6ed1e001a268dc593d011c3375
Steve Tan <i@stevetan.me>
2016-08-23 20:00:51 +0800

    Can’t use base model

--- src/main/java/me/stevetan/sparkpoc/models/User.java
+++ src/main/java/me/stevetan/sparkpoc/models/User.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by stevetan on 23/8/16.
@@ -17,10 +17,22 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "user")
-public class User extends BaseModel {
+@Table(name = "\"user\"")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(name = "name", length = 255)
     private String name;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
 }
