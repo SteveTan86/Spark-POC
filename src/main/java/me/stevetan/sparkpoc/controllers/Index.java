2b11f715b1096b6f5aa4cd4b6431911e6c1b4c4f
Steve Tan <i@stevetan.me>
2016-08-23 20:01:47 +0800

    Switch to JPA

--- src/main/java/me/stevetan/sparkpoc/controllers/Index.java
+++ src/main/java/me/stevetan/sparkpoc/controllers/Index.java
@@ -2,27 +2,27 @@ package me.stevetan.sparkpoc.controllers;
 
 import me.stevetan.sparkpoc.models.User;
 import me.stevetan.sparkpoc.utils.Database;
-import org.hibernate.Session;
 import spark.Request;
 import spark.Response;
+
+import javax.persistence.EntityManager;
 
 /**
  * Created by stevetan on 19/8/16.
  */
 public class Index {
     public static Object handleHelloWorld(Request request, Response response) {
-
-        Session session = Database.getSessionFactory().openSession();
-
-        session.beginTransaction();
+        EntityManager entityManager = Database.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
 
         User user = User
                 .builder()
-                .name("Steve")
+                .name("Hello Adele")
                 .build();
 
-        session.save(user);
-        session.getTransaction().commit();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
 
         return user;
     }
