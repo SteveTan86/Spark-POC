36c23f5f6809bea3cad90b1d07bc1816024e3ba5
Steve Tan <steve.tan@ninjavan.co>
2016-08-29 15:08:14 +0800

    Adding DAO and Service layers

---
+++ src/main/java/me/stevetan/sparkpoc/dao/PeopleDAO.java
@@ -0,0 +1,7 @@
+package me.stevetan.sparkpoc.dao;
+
+/**
+ * Created by stevetan on 29/8/16.
+ */
+public class PeopleDAO {
+}
---
+++ src/main/java/me/stevetan/sparkpoc/service/PeopleService.java
@@ -0,0 +1,7 @@
+package me.stevetan.sparkpoc.service;
+
+/**
+ * Created by stevetan on 29/8/16.
+ */
+public class PeopleService {
+}
